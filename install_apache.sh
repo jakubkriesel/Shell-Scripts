eb2a41ec4b9f91361dfe5094d20437bbbf7c6df2
jakubkriesel <jkr@phatconsulting.de>
2016-08-15 10:22:38 +0200

    Update install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update 
-sudo apt-get install apache2
+sudo apt-get -y install apache2
 sudo service apache2 restart
 sudo chown -R www-data:www-data /var/www/software/config
