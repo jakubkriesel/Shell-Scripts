3f2565b45d2526ced3fa8b45e595daec35381db3
jakubkriesel <jkr@phatconsulting.de>
2016-08-18 13:14:09 +0200

    Update install_mysql.sh

--- install_mysql.sh
+++ install_mysql.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password MySuperPassword'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MySuperPassword'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password Peter654321'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password Peter654321'
 apt-get update
 apt-get install -y mysql-server
